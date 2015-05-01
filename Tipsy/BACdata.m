5bc0a716d8252aaec0f4df705357b933c916984d
Justin Oakes <justin@justinoak.es>
2015-05-01 16:15:27 -0600

    Initial comment. Created BACdata class headers and added App groups

---
+++ Tipsy/BACdata.m
@@ -0,0 +1,13 @@
+//
+//  BACdata.m
+//  Tipsy
+//
+//  Created by Justin Oakes on 5/1/15.
+//  Copyright (c) 2015 Oklasoft. All rights reserved.
+//
+
+#import "BACdata.h"
+
+@implementation BACdata
+
+@end
