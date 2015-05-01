7978c494f51a902e7e83b25731e332f4ff5e3a42
Justin Oakes <justin@justinoak.es>
2015-05-01 16:01:11 -0600

    Initial Commit

---
+++ Tipsy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tipsy
+//
+//  Created by Justin Oakes on 5/1/15.
+//  Copyright (c) 2015 Oklasoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
