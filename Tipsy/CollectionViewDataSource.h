917d9bfe1ed29d3154f68c02290086d04893acb4
Justin Oakes <justin@justinoak.es>
2015-05-08 15:20:43 -0600

    created and hooked up collection view data source and delegate.

---
+++ Tipsy/CollectionViewDataSource.h
@@ -0,0 +1,15 @@
+//
+//  CollectionViewDataSource.h
+//  Tipsy
+//
+//  Created by Justin Oakes on 5/8/15.
+//  Copyright (c) 2015 Oklasoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@import UIKit;
+
+@interface CollectionViewDataSource : NSObject <UICollectionViewDataSource, UICollisionBehaviorDelegate>
+
+@end
---
+++ Tipsy/CollectionViewDataSource.m
@@ -0,0 +1,21 @@
+//
+//  CollectionViewDataSource.m
+//  Tipsy
+//
+//  Created by Justin Oakes on 5/8/15.
+//  Copyright (c) 2015 Oklasoft. All rights reserved.
+//
+
+#import "CollectionViewDataSource.h"
+
+@implementation CollectionViewDataSource
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+}
+
+@end
